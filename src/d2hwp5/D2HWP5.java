cf28bc426b9db2ab45633b06fdf4aac7d9526737
juanv <juanv@DESKTOP-TT7GUJN>
2023-04-09 19:35:14 -0400

    sixth stash

---
+++ src/d2hwp5/D2HWP5.java
@@ -0,0 +1,12 @@
+package d2hwp5;
+
+public class D2HWP5 {
+	public static void main(String[] args) {
+		System.out.println("create a local repository 5");
+		System.out.println("create a remote repository");
+	}
+		public void string (String b) {
+			System.out.println("stash this 6");
+			
+		}
+	}
